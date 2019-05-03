5cbd6ebc9e6d79bbb7d879051f9db3f4bd62df8b
xmit <xmit@xmitdeMacmini.lan>
2019-05-03 21:49:10 +0800

    配置更新

--- .electron-vue/webpack.web.config.js
+++ .electron-vue/webpack.web.config.js
@@ -18,6 +18,20 @@ let webConfig = {
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            }
+          ]
+        })
+      },
+      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
